4dd6901b24046cde5375d2387296439460decd43
usama2333 <usama.wizz@gmail.com>
2023-02-16 11:38:50 +0500

    dynamic styles with css module

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,40 +1,40 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
-const FormControl = styled.div`
-
-
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
 
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  color : ${props => (props.check ? 'red' : 'black')}
-}
-
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid ${props => (props.check ? 'red' : '#ccc')};
-  background : ${props => (props.check ? '#ffd7d7' : 'transparent')};
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
-
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
+//   margin: 0.5rem 0;
 
 
-`;
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   color : ${props => (props.check ? 'red' : 'black')}
+// }
+
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid ${props => (props.check ? 'red' : '#ccc')};
+//   background : ${props => (props.check ? '#ffd7d7' : 'transparent')};
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
+
+// & input:focus {
+//   outline: none;
+//   background: #fad0ec;
+//   border-color: #8b005d;
+// }
+
+
+// `;
 
 
 const CourseInput = props => {
@@ -66,12 +66,15 @@ const CourseInput = props => {
      {/* <FormControl className={!isValid && 'invalid'}> */}
      {/* This is also a way to to add class but we use props in next here */}
 
-    <FormControl check = {!isValid}>
+     {/* <FormControl check = {!isValid}></FormControl> */}
+     {/* It is used for styled.div */}
+
+    <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
 
         <label >Course Goal</label>
         <input  type="text" onChange={goalInputChangeHandler} />
 
-    </FormControl>
+    </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
